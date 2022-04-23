790128166084f093047b079641ba3d0ea64732af
lawrencelee0113 <lawrencelee0113@gmai.com>
2022-04-23 13:35:11 +0800

    google login view

--- static/script.js
+++ static/script.js
@@ -1,40 +1,46 @@
 var account = ""
 var passcode = ""
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    
+}
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log(profile);
+    let id = profile.getId();
+    let name = profile.getName();
+    let img = profile.getImageUrl();
+    let email = profile.getEmail();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    $(".login-space").hide();
+    $.post("/google", {"id":id,"name": name,"img":img,"email":email},
+        function (data, textStatus, jqXHR) {
+            if(data.message == "true"){
+                account = id;
+                passcode = data.passcode;
+                $(".g-signin2").hide();
+                $(".google_sighout").show();
+
+                $(".eportal_login_btn").hide();
+                $('.account_icon').show();
+                
+                $(".account_icon").html(name);
+                
+                alert("登入成功")
+            }else if(data.message == "false"){
+                alert("登入失敗")
+            }
+        },
+        "json"
+    );
+}
 $(document).ready(function () {
-    function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-            console.log('User signed out.');
-        });
-        
-    }
-    function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log(profile);
-        let id = profile.getId();
-        let name = profile.getName();
-        let img = profile.getImageUrl();
-        let email = profile.getEmail();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-        $(".login-space").hide();
-        $.post("/google", {"id":id,"name": name,"img":img,"email":email},
-            function (data, textStatus, jqXHR) {
-                if(data.message == "true"){
-                    account = id;
-                    passcode = data.passcode;
-                    $(".g-signin2").hide();
-                    $(".google_sighout").show();
-                    alert("登入成功")
-                }else if(data.message == "false"){
-                    alert("登入失敗")
-                }
-            },
-            "json"
-        );
-    }
     
     
 });
