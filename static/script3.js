b8c41321558d16ab20d322ec98fb29fa0f9a51a4
lawrencelee0113 <lawrencelee0113@gmai.com>
2022-04-23 17:28:14 +0800

    fff

--- static/script3.js
+++ static/script3.js
@@ -1,15 +1,40 @@
 $(document).ready(function () {
-    $("#default_input").click(function (e) { 
+    $("#empty_input").click(function (e) { 
         e.preventDefault();
-        
+        $("#class_input").val("");
+        $("#name_input").val("");
+        $("#ig_input").val("");
+        $("#introduce_input").val("");
     });
     $("#save_input").click(function (e) { 
         e.preventDefault();
-        $.post("/self_info", {"account":account,"class":$("#class_input").val(),"ig":$("ig_input").val(),"introduce":$("#introduce_input").val(),"name":$("#name_input").val()},
-            function (data, textStatus, jqXHR) {
-                console.log(data)
-            },
-            "json"
-        );
+        if(change_input){
+
+            change_input = false;
+            $("#save_input").html("儲存中...");
+            $.post("/self_info", {"account":account,"class":$("#class_input").val(),"ig":$("ig_input").val(),"introduce":$("#introduce_input").val(),"name":$("#name_input").val()},
+                function (data, textStatus, jqXHR) {
+                    if(data.message == "true") {
+    
+                        $("#save_input").html("已儲存！");
+                    }else if(data.message == "false"){
+                        $("#save_input").html("儲存失敗.");
+                        setTimeout(function(){
+                            $("#save_input").html("儲存資料！");
+                            
+                        },1000)
+    
+                    }
+                },
+                "json"
+            );
+        }
+    });
+    var change_input = false;
+    $(".login_btns .q_item input").change(function (e) { 
+        e.preventDefault();
+        change_input = true;
+        $("#save_input").html("儲存資料！");
+        
     });
 });
